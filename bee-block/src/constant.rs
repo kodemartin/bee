2429d29752f5af4df538e3ddb6585db9cd6f5b8a
Thoralf Müller <thoralf.mue@gmail.com>
2024-03-15 16:29:55 +0100

    Increase token supply because different networks can have higher values

--- bee-block/src/constant.rs
+++ bee-block/src/constant.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /// The total number of tokens in circulation.
-pub const TOKEN_SUPPLY: u64 = 2_779_530_283_277_761;
+pub const TOKEN_SUPPLY: u64 = u64::MAX;
 
 /// Protocol version currently used by the network.
 pub const PROTOCOL_VERSION: u8 = 2;
